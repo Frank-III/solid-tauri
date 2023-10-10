cbec2a5fd4f02c6474b2dbee9841ee4675853a4e
Frank Wang <frankwaang@outlook.com>
2023-10-10 18:02:42 -0500

    setup everything

---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig } from 'vite'
+import UnoCSS from 'unocss/vite'
+import presetIcons from '@unocss/preset-icons'
+import presetUno from '@unocss/preset-uno'
+import presetAttributify from '@unocss/preset-attributify'
+import * as path from "path"
+import solidPlugin from 'vite-plugin-solid'
+
+// https://vitejs.dev/config/
+export default defineConfig(async () => ({
+  plugins: [
+    solidPlugin(),
+    UnoCSS({
+      presets: [
+        presetUno(),
+        presetAttributify(),
+        presetIcons({
+          extraProperties: {
+            'display': 'inline-block',
+            'vertical-align': 'middle',
+          },
+        }),
+      ],
+    }),
+  ],
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+  },
+  // 3. to make use of `TAURI_DEBUG` and other env variables
+  // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
+  envPrefix: ['VITE_', 'TAURI_'],
+  resolve: {
+      alias: {
+        "@/": path.join(__dirname, "src/"),
+      },
+  },
+}))
